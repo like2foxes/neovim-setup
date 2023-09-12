9ab4ad5c9f0729a6c28870df0c837a624c930db5
Shai Cioara <shai@mreshet.co.il>
2023-09-12 22:30:10 +0300

    Switch to tokyonight

--- lua/lazy-config.lua
+++ lua/lazy-config.lua
@@ -17,11 +17,11 @@ require("lazy").setup({
 		dependencies = { 'nvim-lua/plenary.nvim' }
 	},
 	{
-		"catppuccin/nvim",
-		name = "catppuccin",
+		"folke/tokyonight.nvim",
+		lazy = false,
 		priority = 1000,
 		config = function()
-			vim.cmd('colorscheme catppuccin')
+			vim.cmd('colorscheme tokyonight-night')
 		end
 	},
 	{
