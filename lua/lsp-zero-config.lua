2356e3bcb2eaff0fa1cae154a746e2fd779ec162
Shai Cioara <shai@mreshet.co.il>
2023-09-14 16:17:52 +0300

    Added some icons for lsp signs

--- lua/lsp-zero-config.lua
+++ lua/lsp-zero-config.lua
@@ -1,12 +1,19 @@
 local lsp = require('lsp-zero').preset({})
 
 lsp.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp.default_keymaps({
-	  buffer = bufnr,
-	  preserve_mappings = false
-  })
+	-- see :help lsp-zero-keybindings
+	-- to learn the available actions
+	lsp.default_keymaps({
+		buffer = bufnr,
+		preserve_mappings = false
+	})
+	lsp.set_sign_icons(
+		{
+			error = '✘',
+			warn = '▲',
+			hint = '⚑',
+			info = '»'
+		})
 end)
 
 -- (Optional) Configure lua language server for neovim
@@ -19,15 +26,15 @@ local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
 
 cmp.setup({
-  mapping = {
-    -- `Enter` key to confirm completion
-    ['<CR>'] = cmp.mapping.confirm({select = false}),
+	mapping = {
+		-- `Enter` key to confirm completion
+		['<CR>'] = cmp.mapping.confirm({ select = false }),
 
-    -- Ctrl+Space to trigger completion menu
-    ['<C-Space>'] = cmp.mapping.complete(),
+		-- Ctrl+Space to trigger completion menu
+		['<C-Space>'] = cmp.mapping.complete(),
 
-    -- Navigate between snippet placeholder
-    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-  }
+		-- Navigate between snippet placeholder
+		['<C-f>'] = cmp_action.luasnip_jump_forward(),
+		['<C-b>'] = cmp_action.luasnip_jump_backward(),
+	}
 })
