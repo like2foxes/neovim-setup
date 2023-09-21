398bad0d19ad8be4cd452577569c547af61ff60c
Shai Cioara <shai@mreshet.co.il>
2023-09-21 14:34:48 +0300

    switch to non relative numbers when on command mode

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 require("options")
 require("mappings")
+require("autocommands")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
---
+++ lua/autocommands.lua
@@ -0,0 +1,14 @@
+vim.api.nvim_create_autocmd({"CmdlineEnter"}, {
+	group = vim.api.nvim_create_augroup("cmd-line-relnum-toggle", { clear = true }),
+	callback = function()
+		vim.wo.relativenumber = false
+		vim.cmd [[ redraw ]]
+	end
+})
+vim.api.nvim_create_autocmd({"CmdlineLeave"}, {
+	callback = function()
+		vim.wo.relativenumber = true
+		vim.cmd [[ redraw ]]
+	end
+})
+
