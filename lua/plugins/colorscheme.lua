2e880b67135ed7b273dbafe914edc49351f5c36d
Shai Cioara <shai@mreshet.co.il>
2023-09-28 00:07:54 +0300

    removed some colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -15,28 +15,8 @@ return {
 		priority = 1000,
 	},
 	{
-		"Mofiqul/dracula.nvim",
-		lazy = false,
-		priority = 1000,
-	},
-	{
-		"rose-pine/neovim",
-		lazy = false,
-		priority = 1000,
-	},
-	{
-		"neanias/everforest-nvim",
-		lazy = false,
-		priority = 1000,
-	},
-	{
-		"shaunsingh/nord.nvim",
-		lazy = false,
-		priority = 1000,
-	},
-	{
 		"rebelot/kanagawa.nvim",
 		lazy = false,
 		priority = 1000
-	}
+	},
 }
